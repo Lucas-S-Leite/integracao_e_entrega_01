14be6032fb473a03875545c189b9ce6f81b13f8c
unknown <celsleite@gmail.com>
2024-03-21 10:41:38 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/calculadora/calculadora_activity.java
@@ -0,0 +1,13 @@
+package com.example.calculadora;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class calculadora_activity  extends AppCompatActivity {
+    @Override
+    public void onCreate (Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.layout_calculadora);
+    }
+}
